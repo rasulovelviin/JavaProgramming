53fb1a4040f89d50579e0331b6f0a2c212bf6aad
rasulovelviin <rasulovelviin@gmail.com>
2022-04-06 01:19:17 -0400

    updated_19 weeks

---
+++ https:/github.com/TuncSamli/SeleniumProject25/src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_actions.java
@@ -0,0 +1,40 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T3_actions {
+
+    @Test
+    public void hovering_test(){
+
+
+        //TC #3: Hover Test
+//        1. Go to https://practice.cydeo.com/hovers
+
+        Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+
+//        2. Hover over to first image
+
+
+
+
+//        3. Assert:
+//        a. “name: user1” is displayed
+//        b. “view profile” is displayed
+//        4. Hover over to second image
+//        5. Assert:
+//        a. “name: user2” is displayed
+//        b. “view profile” is displayed
+//        6. Hover over to third image
+//        7. Confirm:
+//        a. “name: user3” is displayed
+//        b. “view profile” is displayed
+
+
+
+    }
+}
+
